bab963cd388d0fb591f230a84d5f48a0e6cdb7c8
Julian Gracin <gracin.julian@gmail.com>
2021-12-08 22:49:47 +0100

    solve Day06 part2

--- src/day06/Main.kt
+++ src/day06/Main.kt
@@ -3,16 +3,16 @@ package day06
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        val lanternFish = input[0]
-            .split(",")
-            .map { LanternFish(it.toByte()) }
+    fun part1(input: List<String>): Long {
+        val fish = input[0].split(",").map { it.toInt() }
 
-        return simulateLanternFishReproduction(lanternFish)
+        return simulateReproduction(fish)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val fish = input[0].split(",").map { it.toInt() }
+
+        return simulateReproduction(fish, 256)
     }
 
     val input = readInput("day06/input")
@@ -20,34 +20,47 @@ fun main() {
     println("part2: ${part2(input)}")
 }
 
-class LanternFish(
-    var internalTimer: Byte = 8,
-) {
-    override fun toString() = "$internalTimer"
+const val MATURE_FISH = 6
+const val YOUNG_FISH = 8
 
-    fun reduceInternalTimer(): Boolean {
-        if (internalTimer == 0.toByte()) {
-            internalTimer = 6
-            return true
-        }
+fun simulateReproduction(initialFish: List<Int>, days: Int = 80): Long {
+    val fishMap = mutableMapOf(
+        Pair(0, 0L),
+        Pair(1, 0L),
+        Pair(2, 0L),
+        Pair(3, 0L),
+        Pair(4, 0L),
+        Pair(5, 0L),
+        Pair(6, 0L),
+        Pair(7, 0L),
+        Pair(8, 0L),
+    )
 
-        internalTimer--
-
-        return false
-    }
-}
-
-fun simulateLanternFishReproduction(initialFish: List<LanternFish>, days: Int = 80): Int {
-    val lanternFish = initialFish.toMutableList()
-
-    for (day in 1..days) {
-        val newFish = mutableListOf<LanternFish>()
-        for (fish in lanternFish) {
-            if (fish.reduceInternalTimer()) newFish += LanternFish()
-        }
-
-        lanternFish.addAll(newFish)
+    for (fish in initialFish) {
+        val fishCount = fishMap.getOrDefault(fish, 0)
+        fishMap[fish] = fishCount + 1
     }
 
-    return lanternFish.size
+    for(day in 1 .. days) {
+        var newFish = 0L
+        var matureFish = 0L
+        for (currentTimer in 0 .. 8) {
+            if (currentTimer == 0) {
+                newFish = fishMap[0]!!
+                matureFish = fishMap[0]!!
+
+                fishMap[0] = 0
+                continue
+            }
+
+            val previousTimer = currentTimer - 1
+            fishMap[previousTimer] = fishMap[previousTimer]!! + fishMap[currentTimer]!!
+            fishMap[currentTimer] = 0
+        }
+
+        fishMap[YOUNG_FISH] = newFish
+        fishMap[MATURE_FISH] = fishMap[MATURE_FISH]!! + matureFish
+    }
+
+    return fishMap.toList().sumOf { (_, u) -> u }
 }

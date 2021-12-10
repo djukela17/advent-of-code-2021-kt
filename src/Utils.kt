9925166ce2ba3e5527c2c18489dc7458328a6724
Julian Gracin <gracin.julian@gmail.com>
2021-12-10 19:46:11 +0100

    add helper function for measuring time

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,7 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.*
 
 /**
  * Reads lines from the given input txt file.
@@ -11,3 +12,12 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Measures the time taken by function
+ */
+fun measure(block: () -> Unit) {
+    val start = Date()
+    block()
+    println("time: ${Date().time - start.time}ms")
+}
